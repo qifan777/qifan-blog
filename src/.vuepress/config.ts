c5b05cc0c637edb5f81a514383afdc7a9bc94850
linjiacheng <1507906763@qq.com>
2024-02-04 19:55:25 +0800

    初始化

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,48 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+import { readmorePlugin } from 'vuepress-plugin-readmore-popular-next'
+
+export default defineUserConfig({
+  base: '/blog/',
+  dest: 'src/.vuepress/blog',
+  lang: "zh-CN",
+  title: "起凡Code闲聊",
+  description: "起凡Code闲聊",
+  theme,
+  plugins: [
+    readmorePlugin({
+      // 已申请的博客 ID
+      blogId: '35697-0711444927073-432',
+      // 已申请的微信公众号名称
+      name: '起凡code闲聊',
+      // 已申请的微信公众号回复关键词
+      keyword: 'AB2331',                    
+      // 已申请的微信公众号二维码图片
+      qrcode: 'https://www.jarcheng.top/blog/qrcode.jpg',
+      // 文章内容的 JS 选择器，若使用的不是官方默认主题，则需要根据第三方的主题来设置
+      selector: 'div.theme-hope-content',
+      // 自定义的 JS 资源链接，可用于 CDN 加速
+      libUrl: 'https://qiniu.techgrow.cn/readmore/dist/readmore.js',
+      // 自定义的 CSS 资源链接，可用于适配不同风格的博客
+      cssUrl: 'https://qiniu.techgrow.cn/readmore/dist/vuepress2.css',
+      // 文章排除添加引流工具的 URL 规则，支持使用路径、通配符、正则表达式的匹配规则
+      excludes: { strExp: [], regExp: [] },
+      // 是否反转 URL 排除规则的配置，即只有符合排除规则的文章才会添加引流工具
+      reverse: false,
+      // 是否添加微信公众号引流工具到移动端页面
+      allowMobile: false,
+      // 文章内容的预览高度(例如 300)
+      height: 'auto',
+      // 文章解锁后凭证的有效天数
+      expires: 365,
+      // 定时校验凭证有效性的时间间隔（秒）
+      interval: 60,
+      // 等待 DOM 节点加载完成的时间（毫秒），如果部分页面的引流功能无法生效，可适当增大此参数的值
+      waitDomMills: 1000,
+      // 每篇文章随机添加引流工具的概率，有效范围在 0.1 ~ 1 之间，1 则表示所有文章默认都自动添加引流工具
+      random: 1
+    })
+  ]
+  // 和 PWA 一起启用
+  // shouldPrefetch: false,
+});
